74c2bd55b56a5fda36496440947600ee54402444
Will Thomas <technimanx@gmail.com>
2025-12-13 00:06:08 +0000

    Day 1 part 2

--- day01.js
+++ day01.js
@@ -1,32 +1,55 @@
 import fs from 'node:fs'
 
+// read input
 const fdata = fs.readFileSync('inputs/01.txt', 'utf8')
 const inputLines = fdata.split('\n')
 
+// init - dial starts at 50
 let dialPosition = 50
-let zeroesCount = 0
+let endOnZeroesCount = 0
+let duringZeroesCount = 0
 
 for (let line of inputLines) {
+	// avoid the empty line at the end of the file
 	if (!line) continue
+
+	// extract the info we need
 	let dir = line[0]
-	let amount = parseInt(line.substring(1))
+	let rotateAmount = parseInt(line.substring(1))
 
-	if (dir === 'R') {
-		dialPosition += amount
-		while (dialPosition > 99) {
-			dialPosition -= 100
+	// turn the dial one click at a time
+	function rotateLeft(amount) {
+		while (amount > 0) {
+			amount--
+			dialPosition--
+			// check if we click zero
+			if (dialPosition === 0) {
+				duringZeroesCount++
+			} else if (dialPosition === -1) {
+				dialPosition = 99
+			}
 		}
-	} else {
-		dialPosition -= amount
-		while (dialPosition < 0) {
-			dialPosition += 100
+	}
+	function rotateRight(amount) {
+		while (amount > 0) {
+			amount--
+			dialPosition++
+			// check if we click zero
+			if (dialPosition === 100) {
+				duringZeroesCount++
+				dialPosition = 0
+			}
 		}
 	}
 
-	if (dialPosition === 0) {
-		zeroesCount++
-	}
-	// console.log(`line: ${line} dialPosition after: ${dialPosition}`)
+	// begin rotating the dial
+	if (dir === 'R') rotateRight(rotateAmount)
+	else rotateLeft(rotateAmount)
+
+	// check if we landed on zero
+	if (dialPosition === 0) endOnZeroesCount++
 }
 
-console.log(`Times dial landed on zero: ${zeroesCount}`)
+// answers!
+console.log(`Times dial ended on zero: ${endOnZeroesCount}`)
+console.log(`Times dial passed zero: ${duringZeroesCount}`)
