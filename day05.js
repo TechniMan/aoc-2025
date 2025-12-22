9ffc9d06cf0ff581c632b64680a8c1e1dff07621
Will Thomas <technimanx@gmail.com>
2025-12-22 01:14:04 +0000

    Cleaned up d5p2

--- day05.js
+++ day05.js
@@ -44,33 +44,33 @@ for (let id of stockIds) {
 console.log(`Fresh stock: ${sumFresh}`)
 
 function combineOverlappingRanges(ranges) {
-	console.log(`Combining ${ranges.length} ranges...`)
+	//console.log(`Combining ${ranges.length} ranges...`)
 	const combinedRanges = []
 	for (let range of ranges) {
 		const beginInsideRangeIdx = combinedRanges.findIndex(cr => cr.begin <= range.begin && range.begin <= cr.end)
 		const beginInsideRange = combinedRanges[beginInsideRangeIdx]
 		const endInsideRangeIdx = combinedRanges.findIndex(cr => cr.begin <= range.end && range.end <= cr.end)
 		const endInsideRange = combinedRanges[endInsideRangeIdx]
-//		console.log(`begIdx: ${beginInsideRangeIdx} begRange: {${beginInsideRange?.begin},${beginInsideRange?.end}} endIdx: ${endInsideRangeIdx} endRange: {${endInsideRange?.begin},${endInsideRange?.end}}`)
+		//console.log(`begIdx: ${beginInsideRangeIdx} begRange: {${beginInsideRange?.begin},${beginInsideRange?.end}} endIdx: ${endInsideRangeIdx} endRange: {${endInsideRange?.begin},${endInsideRange?.end}}`)
 
 		if (beginInsideRangeIdx > -1 && beginInsideRangeIdx === endInsideRangeIdx) {
 			// new range falls entirely in one existing range
 			// so just skip
-			console.log(`range ${rangeToString(range)} entirely inside range ${rangeToString(beginInsideRange)}`)
+			//console.log(`range ${rangeToString(range)} entirely inside range ${rangeToString(beginInsideRange)}`)
 		} else if (beginInsideRange) {
 			// new range beginning falls in an existing range
 			// so extend the end of the range to meet this one
 			combinedRanges[beginInsideRangeIdx] = { begin: beginInsideRange.begin, end: range.end }
-			console.log(`range ${rangeToString(range)} starts in range ${rangeToString(beginInsideRange)}`)
+			//console.log(`range ${rangeToString(range)} starts in range ${rangeToString(beginInsideRange)}`)
 		} else if (endInsideRange) {
 			// new range ending falls in an existing range
 			// so extend the begin of the range to meet this one
 			combinedRanges[endInsideRangeIdx] = { begin: range.begin, end: endInsideRange.end }
-			console.log(`range ${rangeToString(range)} ends in range ${rangeToString(endInsideRange)}`)
+			//console.log(`range ${rangeToString(range)} ends in range ${rangeToString(endInsideRange)}`)
 		} else {
 			// no overlap; add it as a new range
 			combinedRanges.push(range)
-			console.log(`range ${rangeToString(range)} not yet present`)
+			//console.log(`range ${rangeToString(range)} not yet present`)
 		}
 	}
 	console.log(`Collapsed ${ranges.length} to ${combinedRanges.length} ranges`)
@@ -78,28 +78,20 @@ function combineOverlappingRanges(ranges) {
 }
 
 // first, compact down all the ranges
-//console.log(allRanges)
+// sort the ranges in ascending order to make it easier to deal with and more predictable
 allRanges.sort((rangeA, rangeB) => {
 	if (rangeA.begin > rangeB.begin) return 1
 	if (rangeA.begin < rangeB.begin) return -1
 	return 0
 })
 const intermediateRanges = combineOverlappingRanges(allRanges)
-console.log()
-//console.log(intermediateRanges)
 // then, compact again in case there are combined ranges which first didn't overlap but later did
-//let finalRanges = intermediateRanges
-let finalRanges = combineOverlappingRanges(intermediateRanges)
-finalRanges = combineOverlappingRanges(finalRanges)
-finalRanges = combineOverlappingRanges(finalRanges)
-//console.log(finalRanges)
+const finalRanges = combineOverlappingRanges(intermediateRanges)
 
 // sum the ranges by the begin & end
 let idCount = 0n
 for (let r of finalRanges) {
 	// add 1 because the range is inclusive
-	const add = 1n + (r.end > r.begin ? r.end - r.begin : r.begin - r.end)
-	idCount += add
-	if (add < 0n) console.log(`Count after: ${idCount} r: {${r.begin}-${r.end}} add: ${add}`)
+	idCount += 1n + r.end - r.begin
 }
 console.log(`Ids in ranges: ${idCount}`)
