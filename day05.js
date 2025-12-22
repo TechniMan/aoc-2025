7a3cf973eddb07a984d578778f1835bbc897a213
Will Thomas <technimanx@gmail.com>
2025-12-22 01:10:12 +0000

    Completed day 5 part 2

--- day05.js
+++ day05.js
@@ -1,10 +1,16 @@
 import fs from 'node:fs'
 
+Array.prototype.any = Array.prototype.some
+
 const input = fs.readFileSync('inputs/05.txt', 'utf8')
 const inputLines = input.split('\n')
 
-const ranges = []
+const allRanges = []
 const stockIds = []
+
+function rangeToString(range) {
+	return `{ ${range?.begin || 'x'}, ${range?.end || 'x'} }`
+}
 
 // process input
 for (let l = 0; l < inputLines.length; ++l) {
@@ -15,7 +21,7 @@ for (let l = 0; l < inputLines.length; ++l) {
 	// treat as a range
 	if (inputLines[l].indexOf('-') !== -1) {
 		const [ begin, end ] = inputLines[l].split('-')
-		ranges.push({ begin, end })
+		allRanges.push({ begin: BigInt(begin), end: BigInt(end) })
 	}
 	// treat as a stock id
 	else {
@@ -27,7 +33,7 @@ for (let l = 0; l < inputLines.length; ++l) {
 let sumFresh = 0
 for (let id of stockIds) {
 	let fresh = false
-	for (let range of ranges) {
+	for (let range of allRanges) {
 		if (range.begin <= id && id <= range.end) {
 			fresh = true
 			break
@@ -35,5 +41,65 @@ for (let id of stockIds) {
 	}
 	sumFresh += fresh
 }
-
 console.log(`Fresh stock: ${sumFresh}`)
+
+function combineOverlappingRanges(ranges) {
+	console.log(`Combining ${ranges.length} ranges...`)
+	const combinedRanges = []
+	for (let range of ranges) {
+		const beginInsideRangeIdx = combinedRanges.findIndex(cr => cr.begin <= range.begin && range.begin <= cr.end)
+		const beginInsideRange = combinedRanges[beginInsideRangeIdx]
+		const endInsideRangeIdx = combinedRanges.findIndex(cr => cr.begin <= range.end && range.end <= cr.end)
+		const endInsideRange = combinedRanges[endInsideRangeIdx]
+//		console.log(`begIdx: ${beginInsideRangeIdx} begRange: {${beginInsideRange?.begin},${beginInsideRange?.end}} endIdx: ${endInsideRangeIdx} endRange: {${endInsideRange?.begin},${endInsideRange?.end}}`)
+
+		if (beginInsideRangeIdx > -1 && beginInsideRangeIdx === endInsideRangeIdx) {
+			// new range falls entirely in one existing range
+			// so just skip
+			console.log(`range ${rangeToString(range)} entirely inside range ${rangeToString(beginInsideRange)}`)
+		} else if (beginInsideRange) {
+			// new range beginning falls in an existing range
+			// so extend the end of the range to meet this one
+			combinedRanges[beginInsideRangeIdx] = { begin: beginInsideRange.begin, end: range.end }
+			console.log(`range ${rangeToString(range)} starts in range ${rangeToString(beginInsideRange)}`)
+		} else if (endInsideRange) {
+			// new range ending falls in an existing range
+			// so extend the begin of the range to meet this one
+			combinedRanges[endInsideRangeIdx] = { begin: range.begin, end: endInsideRange.end }
+			console.log(`range ${rangeToString(range)} ends in range ${rangeToString(endInsideRange)}`)
+		} else {
+			// no overlap; add it as a new range
+			combinedRanges.push(range)
+			console.log(`range ${rangeToString(range)} not yet present`)
+		}
+	}
+	console.log(`Collapsed ${ranges.length} to ${combinedRanges.length} ranges`)
+	return combinedRanges
+}
+
+// first, compact down all the ranges
+//console.log(allRanges)
+allRanges.sort((rangeA, rangeB) => {
+	if (rangeA.begin > rangeB.begin) return 1
+	if (rangeA.begin < rangeB.begin) return -1
+	return 0
+})
+const intermediateRanges = combineOverlappingRanges(allRanges)
+console.log()
+//console.log(intermediateRanges)
+// then, compact again in case there are combined ranges which first didn't overlap but later did
+//let finalRanges = intermediateRanges
+let finalRanges = combineOverlappingRanges(intermediateRanges)
+finalRanges = combineOverlappingRanges(finalRanges)
+finalRanges = combineOverlappingRanges(finalRanges)
+//console.log(finalRanges)
+
+// sum the ranges by the begin & end
+let idCount = 0n
+for (let r of finalRanges) {
+	// add 1 because the range is inclusive
+	const add = 1n + (r.end > r.begin ? r.end - r.begin : r.begin - r.end)
+	idCount += add
+	if (add < 0n) console.log(`Count after: ${idCount} r: {${r.begin}-${r.end}} add: ${add}`)
+}
+console.log(`Ids in ranges: ${idCount}`)
