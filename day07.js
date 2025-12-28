090d1bfb7654b356e9654c45dcb66a92e745ecba
Will Thomas <technimanx@gmail.com>
2025-12-28 01:07:46 +0000

    Compelted day 7 part 2

--- day07.js
+++ day07.js
@@ -2,29 +2,44 @@ import fs from 'node:fs'
 
 const inputLines = fs.readFileSync('inputs/07.txt', 'utf8').split('\n')
 
+const newRayArray = function() {
+	let obj = {}
+	for (let i = 0 ; i < inputLines[0].length; ++i) {
+		obj[i] = 0
+	}
+	return obj
+}
+
 // keep track of the rays by their X positions on the row
-let rays = []
+let rays = newRayArray()
 let countSplits = 0
-rays.push(inputLines[0].indexOf('S'))
+let rayCount = 0
+rays[inputLines[0].indexOf('S')] = 1
 // keep track of each ray travelling through the manifold
 for (let l = 1; l < inputLines.length - 1; ++l) {
 	const line = inputLines[l]
 	// check what happens to each ray
-	const newRays = []
-	for (const r of rays) {
+	const newRays = newRayArray()
+	for (const col in rays) {
+		const raysInCol = rays[col]
+		// don't work on columns without rays
+		if (raysInCol === 0) { continue }
+
 		// find the char at the index the ray is travelling down
-		const c = line[r]
+		const c = line[col]
 		if (c === '.') {
 			// . is empty space; continue travelling
-			if (newRays.indexOf(r) === -1) newRays.push(r)
+			newRays[col] += raysInCol
 		} else if (c === '^') {
 			// ^ is a splitter; remove this ray and add two more to the sides
 			countSplits++
-			if (newRays.indexOf(r - 1) === -1) newRays.push(r - 1)
-			if (newRays.indexOf(r + 1) === -1) newRays.push(r + 1)
+			newRays[parseInt(col) - 1] += raysInCol
+			newRays[parseInt(col) + 1] += raysInCol
 		}
 	}
 	rays = newRays
 }
 
-console.log(`Total splits: ${countSplits}`)
+console.log(`P1 Splits: ${countSplits}`)
+rayCount += Object.values(rays).reduce((acc, cur) => acc + cur, 0)
+console.log(`Ray count: ${rayCount}`)
